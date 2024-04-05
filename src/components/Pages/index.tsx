7ed1b04761515efa95c59ed6253fe4e71b53d710
Pisodev77 <pisodev0427@gmail.com>
2024-04-05 09:05:02 +0900

    fix: remove unused files

--- src/components/Pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from './Home';
-export { default as AboutPage } from './About';
-export { default as ProjectsPage } from './Projects';
-export { default as BlogPage } from './Blog';
