3a2c7c36ba60717d6fa9eb0d759afb2005128f7d
Pisodev77 <pisodev0427@gmail.com>
2024-04-05 09:00:56 +0900

    docs: Darkmode? Body? Html? useHook? retrospective
    -   The `@layer base` directive does not seem to function as expected.
    -   Manual changes to the `className` of the HTML element occur when toggling dark mode with Tailwind CSS.
    -   Attempting to apply custom theme colors to the `body` tag.
    
    -   There is an issue preventing the successful change of the `className` for the body element. It is crucial to identify the underlying cause.
    -   The project utilizes a custom hook related to dark mode, which may be influencing the `className` of the `body` tag.
    
    -   In `index.css` which is main.css of tailwind. use @layer base for body tag.
    -   Change darkmode class changes from body to html.
    
    -   Why className doesn't change on tailwindcss with usehook?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
 
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class',
-    theme: {
-        colors: {
-            primary: '#FEFFDE',
-            secondary: '#52734D',
-            netural: colors.sky,
-        },
-        extend: {},
-        fontFamily: {
-            ibm: ['IBM', 'sans-serif'],
-        },
-    },
-    plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	// 현재 tailwind 버전으로는 class 3.41이후는 selector
+	darkMode: 'class',
+	theme: {
+		colors: {
+			lightFont: '#000000',
+			lightBg: '#F0F0F0',
+			darkFont: '#E0E0E0',
+			darkBg: '#121212',
+		},
+		fontFamily: {
+			ibm: ['IBM', 'sans-serif'],
+		},
+	},
+
+	plugins: [],
 };
