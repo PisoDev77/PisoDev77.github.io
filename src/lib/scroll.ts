a90ac7c7c947a402d3ec4a7835f97065caf698b1
Pisodev77 <pisodev0427@gmail.com>
2024-04-05 09:06:32 +0900

    fix: remove unused files

--- src/lib/scroll.ts
+++
@@ -1,6 +0,0 @@
-import { gsap } from 'gsap';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
-gsap.registerPlugin(ScrollToPlugin);
-
-export const scrollToPage = (path: string) => gsap.to(window, { scrollTo: { y: '#' + path } });
