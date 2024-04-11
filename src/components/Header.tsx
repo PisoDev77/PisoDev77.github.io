0c3d199224ceda5ae3fe452775ffeaeb00b3708a
Pisodev77 <pisodev0427@gmail.com>
2024-04-11 15:32:43 +0900

    feat: Upload Darkmode svg images
    - Use svg images in darkmode button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,31 @@
 import { useEffect, useState } from 'react';
-import { scrollToPage } from '../lib/scroll';
 
 export default function Header() {
-    const [isDarkmode, setIsDarkmode] = useState(false);
+	const [isDarkmode, setIsDarkmode] = useState(false);
 
-    useEffect(() => {
-        const storedDarkmode = JSON.parse(localStorage.getItem('dark') ?? 'false');
-        setIsDarkmode(storedDarkmode);
-        document.body.classList.toggle('dark', storedDarkmode);
-    }, []);
+	useEffect(() => {
+		const storedDarkmode = JSON.parse(localStorage.getItem('dark') ?? 'false');
+		setIsDarkmode(storedDarkmode);
+		const htmlElement = document.querySelector('html');
+		if (htmlElement) {
+			htmlElement.classList.toggle('dark', storedDarkmode);
+		}
+	}, []);
 
-    const handleDarkmode = () => {
-        localStorage.setItem('dark', JSON.stringify(!isDarkmode));
-        setIsDarkmode(!isDarkmode);
-        document.body.classList.toggle('dark', !isDarkmode);
-    };
+	const handleDarkmode = () => {
+		localStorage.setItem('dark', JSON.stringify(!isDarkmode));
+		setIsDarkmode(!isDarkmode);
+		const htmlElement = document.querySelector('html');
+		if (htmlElement) {
+			htmlElement.classList.toggle('dark', !isDarkmode);
+		}
+	};
 
-    const handleNav = (e: React.MouseEvent<HTMLLIElement>) => {
-        scrollToPage((e.target as HTMLLIElement).dataset.link ?? '');
-    };
-
-    return (
-        <header>
-            <button onClick={handleDarkmode}>Darkmode</button>
-            <nav>
-                <ul>
-                    <li onClick={handleNav} data-link="home">
-                        home
-                    </li>
-                    <li onClick={handleNav} data-link="about">
-                        about
-                    </li>
-                    <li onClick={handleNav} data-link="projects">
-                        projects
-                    </li>
-                    <li onClick={handleNav} data-link="blog">
-                        blog
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+	return (
+		<header className='p-4'>
+			<button onClick={handleDarkmode}>
+				<img className='sm:h-6' src={`/public/images/${isDarkmode ? 'darkmode' : 'lightmode'}.svg`} alt='' />
+			</button>
+		</header>
+	);
 }
