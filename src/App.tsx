6f9b6b09b1c3a2594b46287e7a24ec902337328b
Pisodev77 <pisodev0427@gmail.com>
2024-04-11 15:37:40 +0900

    feat: About section structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Header from './components/Header';
 import Heading1 from './components/Headings/Heading1';
+import ImageCaption from './components/ImageCaption';
 
 function App() {
 	return (
 		<main>
 			<Header />
-			<Heading1>About</Heading1>
+			<section className='grid grid-cols-1 sm:grid-cols-2'>
+				<Heading1>About</Heading1>
+				<ImageCaption name='communication' />
+				<ImageCaption name='readability' />
+				<ImageCaption name='user-friendly' />
+				<ImageCaption name='intuition' />
+			</section>
 		</main>
 	);
 }
