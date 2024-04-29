dd2598dc9e0d7f6e0a2421cf2f91859263f0f227
Pisodev77 <pisodev0427@gmail.com>
2024-04-29 11:47:50 +0900

    conf: rollback config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	base: '/',
-	// optimizeDeps: {
-	//     exclude: ['js-big-decimal'],
-	// },
 	server: {
-		proxy: {
-			'/testNotion': 'https://api.notion.com/v1/databases/e543782682fe480db5f4ffe78a11148e/query',
-			changeOrigin: true,
-		},
+		proxy: {},
 	},
 });
