75e843e24929c2e64198e37e76be9268bd669fc9
Pisodev77 <pisodev0427@gmail.com>
2024-08-05 13:50:07 +0900

    feat: add github and page link in project section

--- src/components/Sections/ProjectSection.tsx
+++ src/components/Sections/ProjectSection.tsx
@@ -3,14 +3,23 @@ import { formatDate } from '../../lib/date';
 import Heading3 from '../Headings/Heading3';
 
 import { projectType } from '../../mock/project';
+import { GithubSvg, ShowSvg } from '../Svg';
 
 export default function ProjectSection(props: projectType) {
-	const { title, startDate, endDate } = props;
+	const { title, startDate, endDate, github, page } = props;
 
 	return (
 		<section>
 			<Heading3>{title}</Heading3>
 			<b>{formatDate(startDate) + ' ~ ' + formatDate(endDate)}</b>
+			<ul className='flex'>
+				<li className='p-4' onClick={() => window.open(github, '_blank')}>
+					{GithubSvg}
+				</li>
+				<li className='p-4' onClick={() => window.open(page, '_blank')}>
+					{ShowSvg}
+				</li>
+			</ul>
 		</section>
 	);
 }
