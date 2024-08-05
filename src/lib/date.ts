537178468ad46809a2232a78135313b66a5c2476
Pisodev77 <pisodev0427@gmail.com>
2024-08-05 13:39:12 +0900

    fix: month -> date

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,6 @@
 const formatDate = (date: Date) =>
-	`${(date.getFullYear() + '').substring(2)}-${date.getMonth() + 1}-${date.getMonth()}`;
+	`${(date.getFullYear() + '').substring(2)}-${(date.getMonth() + 1 + '').padStart(2, '0')}-${(
+		date.getDate() + ''
+	).padStart(2, '0')}`;
 
 export { formatDate };
